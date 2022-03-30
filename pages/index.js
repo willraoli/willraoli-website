cf46f63ea5364f7bce32d0714d2761bccebbc234
willraoli <william.christopher.96@gmail.com>
2022-03-30 01:15:45 -0300

    fix: crossorigin = crossOrigin

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="normalize.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
       </Head>
       <main>
