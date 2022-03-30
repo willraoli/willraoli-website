cdfdc7c017c823ea048c1191ee6528139f5a3584
willraoli <william.christopher.96@gmail.com>
2022-03-29 22:39:37 -0300

    feat: added contact page

---
+++ pages/contact/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+function contact() {
+  return (
+    <>
+      <Head>
+        <title>willraoli@contact</title>
+      </Head>
+      <div>contact</div>
+    </>
+  )
+}
+
+export default contact
