a594dc78bfd29e0a36d066de803b4da27c48e3f0
willraoli <william.christopher.96@gmail.com>
2022-03-30 01:06:58 -0300

    style: added css for posts

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,9 @@
 import Link from "next/link"
+import styles from "../styles/Home.module.css"
 
 function Post({ post }) {
   return (
-    <div>
+    <div className={styles.post}>
       <h1>{post.frontmatter.title}</h1>
       <p>{post.frontmatter.excerpt}</p>
       <Link href={`/blog/${post.slug}`}>
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,20 @@ import path from "path"
 import matter from "gray-matter"
 import Post from "../components/Post"
 
+import styles from "../styles/Home.module.css"
+
 export default function Home({ posts }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>willraoli@home</title>
         <meta name="description" content="willraoli's website" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="normalize.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
       </Head>
-
       <main>
         <div>
           {posts.map((post) => (
@@ -20,12 +25,17 @@ export default function Home({ posts }) {
           ))}
         </div>
       </main>
-      <aside>
+      <aside className={styles.aside}>
         <div>
-          <h1>willraoli</h1>
-          <p>willraoli&#39s website</p>
+          <h1>William Oliveira</h1>
+          <p>Descrição breve</p>
         </div>
       </aside>
+      <footer className={styles.footer}>
+        <div>
+          <p>willraoli@footer</p>
+        </div>
+      </footer>
     </div>
   )
 }
@@ -34,9 +44,8 @@ export async function getStaticProps() {
   const files = fs.readdirSync(path.join("posts"))
   const posts = files.map((filename) => {
     const slug = filename.replace('.md', '')
-
     const markdownWithMeta = fs.readFileSync(path.join("posts", filename), 'utf8')
-
+    
     const {data: frontmatter} = matter(markdownWithMeta)
 
     return {
