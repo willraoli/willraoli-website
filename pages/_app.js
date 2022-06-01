3a8cdbce828138ea76c6ab683b2b6f2c2778c82c
William Oliveira <william.christopher.96@gmail.com>
2022-05-31 21:58:27 -0300

    feat: added chakra provider and contentcontainer component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { ChakraProvider, Container } from "@chakra-ui/react"
+import Navbar from "../components/Navbar"
+
+function ContentContainer({ children }) {
+  return (
+    <Container maxW={"3xl"}>
+      {children}
+    </Container>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Navbar />
+      <ContentContainer>
+        <Component {...pageProps} />
+      </ContentContainer>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
