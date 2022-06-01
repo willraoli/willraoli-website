ce49d62c0cb2765e5662c54d3e89f69194015ee3
William Oliveira <william.christopher.96@gmail.com>
2022-05-31 21:58:39 -0300

    refactor: new homepage

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,35 @@
 import Head from "next/head"
-import Image from "next/image"
-import fs from "fs"
-import path from "path"
-import matter from "gray-matter"
-import Post from "../components/Post"
+import { Box, Heading, Image, Stack, Text, useColorModeValue } from "@chakra-ui/react"
 
-import styles from "../styles/Home.module.css"
-
-export default function Home({ posts }) {
+export default function Home() {
+  const DESCRIPTION = {
+    "pt-BR": "Desenvolvedor web focado em tecnologias modernas, otimização de SEO e clean code.",
+    "en-US": "Full-Stack Developer, focused on modern technologies and with focus on software development.",
+  }
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>willraoli@home</title>
         <meta name="description" content="willraoli's website" />
       </Head>
-      <header className={styles.header}>Yo soy el cabezador</header>
-      <main className={`${styles.main} ${styles.wrapper}`}>
-        <h1>Confira meus posts mais recentes</h1>
-        <div>
-          {posts.map((post) => (
-            <Post post={post} key={post.title} />
-          ))}
-        </div>
-      </main>
-      <aside className={styles.aside}>
-        <div className={styles.description}>
-          <Image src="/avatar.jpg" className={styles.avatar} width="100" height="100" alt="Profile pic" />
-          <h1>William Oliveira</h1>
-          <p>Descrição breve</p>
-          <p>Random quote</p>
-          <p>Redes sociais</p>
-        </div>
-      </aside>
-      <footer className={styles.footer}>
-        <div>
-          <p>willraoli@footer</p>
-        </div>
-      </footer>
-    </div>
+      <Stack
+        marginTop={{ base: 16, md: 48 }}
+        as={Box}
+        align={"center"}
+        textAlign={"center"}
+        spacing={{ base: 4, md: 6 }}
+      >
+        <Image src={"avatar.jpg"} borderRadius={"50%"} width={"25%"} />
+        <Heading fontWeight={700} fontSize={{ base: 24, md: 48 }} as={"h1"}>
+          Olá, eu sou{" "}
+          <Text as={"span"} color={useColorModeValue("purple.500", "purple.400")}>
+            Will.
+          </Text>
+        </Heading>
+        <Text>
+          {DESCRIPTION["pt-BR"]}
+        </Text>
+      </Stack>
+    </>
   )
-}
-
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("posts"))
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "")
-    const filepath = path.join("posts", filename)
-    const markdownWithMeta = fs.readFileSync(filepath, "utf8")
-    const { data: frontmatter } = matter(markdownWithMeta)
-
-    return {
-      slug,
-      frontmatter,
-    }
-  })
-
-  return {
-    props: {
-      posts,
-    },
-  }
 }
